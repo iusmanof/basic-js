cf8b234feb703e720d94e71ba9cc81c5fc7fde8f
iusmanof <74846667+iusmanof@users.noreply.github.com>
2021-03-10 09:52:21 +0300

    feat: implpemnt vigenere-ciper function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  type;
+  constructor(type = true) {
+    this.type = type;
+  }
+
+  isReverse(result) {
+    return this.type ? result : result.split("").reverse().join("");
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("One or both param is/are UNDEFINED");
+
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let result = "";
+
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    let i_key = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      // check letters in range
+      if (message[i].match("[A-Z]")) {
+        result += String.fromCharCode(
+          ((message[i].charCodeAt() + key[i_key].charCodeAt()) % 26) + 65
+        );
+        i_key++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.isReverse(result);
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error("One or both param is/are UNDEFINED");
+
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let result = "";
+
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    let i_key = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      // check letters in range
+      if (message[i].match("[A-Z]")) {
+        result += String.fromCharCode(
+          ((message[i].charCodeAt() + 26 - key[i_key].charCodeAt()) % 26) + 65
+        );
+        i_key++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.isReverse(result);
   }
 }
 
