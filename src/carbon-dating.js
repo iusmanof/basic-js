a9ba0b90c243b2802ad173ce0625674364984dcf
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2021-03-24 11:36:52 +0300

    feat: rewrite code in if else

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,15 +4,12 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (
-    typeof sampleActivity == "number" &&
-    sampleActivity instanceof Number &&
-    +sampleActivity < 15 &&
-    sampleActivity > 0
+    typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY
   ) {
-    const k = 0.693 / 5730;
-    const t = Math.ceil(Math.log(15 / +sampleActivity) / k);
-    return t;
+    return false
   } else {
-    return false;
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    const t = Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / k)
+    return t;
   }
 };
